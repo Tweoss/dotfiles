5b9c6407c1fdba9929a3f1924a07874d053c954c
Francis Chua <fqchua@stanford.edu>
2023-02-12 14:03:49 -0800

    added mnemonic comments, removed unnecessary snips

--- obsidian/default_snippets.ts
+++ obsidian/default_snippets.ts
@@ -1,7 +1,10 @@
 [
     // Math mode
+    //! MaKe math
     { trigger: "mk", replacement: "$$0$", options: "tA" },
+    //! Display Math
     { trigger: "dm", replacement: "$$\n$0\n$$", options: "tAw" },
+    // ! BEGin
     { trigger: "beg", replacement: "\\begin{$0}\n$1\n\\end{$0}", options: "mA" },
 
 
@@ -43,7 +46,7 @@
     { trigger: "ome", replacement: "\\omega", options: "mA" },
     { trigger: "@o", replacement: "\\omega", options: "mA" },
     { trigger: "@O", replacement: "\\Omega", options: "mA" },
-    { trigger: "([^\\\\])(${GREEK}|${SYMBOL})", replacement: "[[0]]\\[[1]]", options: "rmA", description: "Add backslash before greek letters and symbols" },
+    // { trigger: "([^\\\\])(${GREEK}|${SYMBOL})", replacement: "[[0]]\\[[1]]", options: "rmA", description: "Add backslash before greek letters and symbols" },
 
 
     // Insert space after greek letters and symbols, etc
@@ -61,26 +64,37 @@
 
 
     // Operations
+    //! TExt
     { trigger: "te", replacement: "\\text{$0}", options: "m" },
     { trigger: "text", replacement: "\\text{$0}", options: "mA" },
+    //! BoldFace
     { trigger: "bf", replacement: "\\mathbf{$0}", options: "mA" },
-    { trigger: "sr", replacement: "^{2}", options: "mA" },
+    //! SQuare
+    { trigger: "sq", replacement: "^{2}", options: "mA" },
+    //! CuBe
     { trigger: "cb", replacement: "^{3}", options: "mA" },
+    //! Raise to Degree
     { trigger: "rd", replacement: "^{$0}$1", options: "mA" },
     { trigger: "_", replacement: "_{$0}$1", options: "mA" },
+    //! SubscripTS
     { trigger: "sts", replacement: "_\\text{$0}", options: "rmA" },
-    { trigger: "sq", replacement: "\\sqrt{ $0 }$1", options: "mA" },
+    //! Square Root
+    { trigger: "sr", replacement: "\\sqrt{ $0 }$1", options: "mA" },
     { trigger: "//", replacement: "\\frac{$0}{$1}$2", options: "mA" },
     { trigger: "ee", replacement: "e^{ $0 }$1", options: "mA" },
+    //! Roman Mode
     { trigger: "rm", replacement: "\\mathrm{$0}$1", options: "mA" },
+    //! CONJugate
     { trigger: "conj", replacement: "^{*}", options: "mA" },
     { trigger: "trace", replacement: "\\mathrm{Tr}", options: "mA" },
     { trigger: "det", replacement: "\\det", options: "mA" },
-    { trigger: "re", replacement: "\\mathrm{Re}", options: "mA" },
-    { trigger: "im", replacement: "\\mathrm{Im}", options: "mA" },
+    // { trigger: "re", replacement: "\\mathrm{Re}", options: "mA" },
+    // { trigger: "im", replacement: "\\mathrm{Im}", options: "mA" },
 
+    //! ,. or ., suffix gives Bold text
     { trigger: "([a-zA-Z]),\\.", replacement: "\\mathbf{[[0]]}", options: "rmA" },
     { trigger: "([a-zA-Z])\\.,", replacement: "\\mathbf{[[0]]}", options: "rmA" },
+    //! text# gives auto subscript text_# with up to two digits
     { trigger: "([A-Za-z])(\\d)", replacement: "[[0]]_{[[1]]}", options: "rmA", description: "Auto letter subscript", priority: -1 },
     { trigger: "([A-Za-z])_(\\d\\d)", replacement: "[[0]]_{[[1]]}", options: "rmA" },
     { trigger: "\\hat{([A-Za-z])}(\\d)", replacement: "hat{[[0]]}_{[[1]]}", options: "rmA" },
@@ -112,19 +126,20 @@
     { trigger: "K", replacement: "\\cancelto{ $0 }{ ${VISUAL} }", options: "mA" },
     { trigger: "S", replacement: "\\sqrt{ ${VISUAL} }", options: "mA" },
 
-
-
     // Symbols
+    //! OOO is infinity
     { trigger: "ooo", replacement: "\\infty", options: "mA" },
     { trigger: "sum", replacement: "\\sum", options: "mA" },
     { trigger: "prod", replacement: "\\prod", options: "mA" },
     { trigger: "lim", replacement: "\\lim_{ ${0:n} \\to ${1:\\infty} } $2", options: "mA" },
+    //! Plus Minus
     { trigger: "pm", replacement: "\\pm", options: "m" },
     { trigger: "mp", replacement: "\\mp", options: "m" },
     { trigger: "...", replacement: "\\dots", options: "mA" },
     { trigger: "<->", replacement: "\\leftrightarrow ", options: "mA" },
     { trigger: "->", replacement: "\\to", options: "mA" },
     { trigger: "!>", replacement: "\\mapsto", options: "mA" },
+    //! INVerSe
     { trigger: "invs", replacement: "^{-1}", options: "mA" },
     { trigger: "\\\\\\", replacement: "\\setminus", options: "mA" },
     { trigger: "||", replacement: "\\mid", options: "mA" },
@@ -138,7 +153,7 @@
     { trigger: "iff", replacement: "\\iff", options: "mA" },
     { trigger: "e\\xi sts", replacement: "\\exists", options: "mA", priority: 1 },
     { trigger: "===", replacement: "\\equiv", options: "mA" },
-    { trigger: "Sq", replacement: "\\square", options: "mA" },
+    // { trigger: "Sq", replacement: "\\square", options: "mA" },
     { trigger: "!=", replacement: "\\neq", options: "mA" },
     { trigger: ">=", replacement: "\\geq", options: "mA" },
     { trigger: "<=", replacement: "\\leq", options: "mA" },
@@ -146,27 +161,31 @@
     { trigger: "<<", replacement: "\\ll", options: "mA" },
     { trigger: "~~", replacement: "\\sim", options: "mA" },
     { trigger: "\\sim ~", replacement: "\\approx", options: "mA" },
-    { trigger: "prop", replacement: "\\propto", options: "mA" },
+    // { trigger: "prop", replacement: "\\propto", options: "mA" },
     { trigger: "nabl", replacement: "\\nabla", options: "mA" },
     { trigger: "del", replacement: "\\nabla", options: "mA" },
     { trigger: "xx", replacement: "\\times", options: "mA" },
+    // two *'s become cdot
     { trigger: "**", replacement: "\\cdot", options: "mA" },
-    { trigger: "para", replacement: "\\parallel", options: "mA" },
+    // { trigger: "para", replacement: "\\parallel", options: "mA" },
 
 
     { trigger: "xnn", replacement: "x_{n}", options: "mA" },
-    { trigger: "xii", replacement: "x_{i}", options: "mA" },
+    { trigger: "xii", replacement: "x_{i}", options: "mA", priority: 1 },
     { trigger: "xjj", replacement: "x_{j}", options: "mA" },
+    // X PLus 1
     { trigger: "xp1", replacement: "x_{n+1}", options: "mA" },
     { trigger: "ynn", replacement: "y_{n}", options: "mA" },
     { trigger: "yii", replacement: "y_{i}", options: "mA" },
     { trigger: "yjj", replacement: "y_{j}", options: "mA" },
 
 
+    //! Math CALligraphy
     { trigger: "mcal", replacement: "\\mathcal{$0}$1", options: "mA" },
+    //! Math BlackBoard
     { trigger: "mbb", replacement: "\\mathbb{$0}$1", options: "mA" },
     { trigger: "ell", replacement: "\\ell", options: "mA" },
-    { trigger: "lll", replacement: "\\ell", options: "mA" },
+    // { trigger: "lll", replacement: "\\ell", options: "mA" },
     { trigger: "LL", replacement: "\\mathcal{L}", options: "mA" },
     { trigger: "HH", replacement: "\\mathcal{H}", options: "mA" },
     { trigger: "CC", replacement: "\\mathbb{C}", options: "mA" },
@@ -192,9 +211,9 @@
 
 
     // Derivatives
-    { trigger: "par", replacement: "\\frac{ \\partial ${0:y} }{ \\partial ${1:x} } $2", options: "m" },
-    { trigger: "pa2", replacement: "\\frac{ \\partial^{2} ${0:y} }{ \\partial ${1:x}^{2} } $2", options: "mA" },
-    { trigger: "pa3", replacement: "\\frac{ \\partial^{3} ${0:y} }{ \\partial ${1:x}^{3} } $2", options: "mA" },
+    { trigger: "par", replacement: "\\frac{\\partial ${0:y}}{\\partial ${1:x}} $2", options: "m" },
+    { trigger: "pa2", replacement: "\\frac{\\partial^{2} ${0:y}}{\\partial ${1:x}^{2}} $2", options: "mA" },
+    { trigger: "pa3", replacement: "\\frac{\\partial^{3} ${0:y}}{\\partial ${1:x}^{3}} $2", options: "mA" },
     { trigger: "pa([A-Za-z])([A-Za-z])", replacement: "\\frac{ \\partial [[0]] }{ \\partial [[1]] } ", options: "rm" },
     { trigger: "pa([A-Za-z])([A-Za-z])([A-Za-z])", replacement: "\\frac{ \\partial^{2} [[0]] }{ \\partial [[1]] \\partial [[2]] } ", options: "rm" },
     { trigger: "pa([A-Za-z])([A-Za-z])2", replacement: "\\frac{ \\partial^{2} [[0]] }{ \\partial [[1]]^{2} } ", options: "rmA" },
@@ -205,43 +224,50 @@
 
 
     // Integrals
+    //! 0 to infinity
     { trigger: "oinf", replacement: "\\int_{0}^{\\infty} $0 \\, d${1:x} $2", options: "mA" },
+    //! -infinity to infinity
     { trigger: "infi", replacement: "\\int_{-\\infty}^{\\infty} $0 \\, d${1:x} $2", options: "mA" },
+    //! Delimited integral (with limits)
     { trigger: "dint", replacement: "\\int_{${0:0}}^{${1:\\infty}} $2 \\, d${3:x} $4", options: "mA" },
+    //! clOsed curve (contour) integral 
     { trigger: "oint", replacement: "\\oint", options: "mA" },
+    //! III (triple integral)
     { trigger: "iiint", replacement: "\\iiint", options: "mA" },
+    //! II (double integral)
     { trigger: "iint", replacement: "\\iint", options: "mA" },
+    //! basic integral
     { trigger: "int", replacement: "\\int $0 \\, d${1:x} $2", options: "mA" },
 
 
 
     // Physics
-    { trigger: "kbt", replacement: "k_{B}T", options: "mA" },
+    // { trigger: "kbt", replacement: "k_{B}T", options: "mA" },
 
 
     // Quantum mechanics
-    { trigger: "hba", replacement: "\\hbar", options: "mA" },
-    { trigger: "dag", replacement: "^{\\dagger}", options: "mA" },
+    // { trigger: "hba", replacement: "\\hbar", options: "mA" },
+    // { trigger: "dag", replacement: "^{\\dagger}", options: "mA" },
     { trigger: "o+", replacement: "\\oplus ", options: "mA" },
     { trigger: "ox", replacement: "\\otimes ", options: "mA" },
-    { trigger: "ot\\mathrm{Im}es", replacement: "\\otimes ", options: "mA" }, // Handle conflict with "im" snippet
-    { trigger: "bra", replacement: "\\bra{$0} $1", options: "mA" },
-    { trigger: "ket", replacement: "\\ket{$0} $1", options: "mA" },
-    { trigger: "brk", replacement: "\\braket{ $0 | $1 } $2", options: "mA" },
-    { trigger: "\\\\bra{([^|]+)\\|", replacement: "\\braket{ [[0]] | $0 ", options: "rmA", description: "Convert bra into braket" },
-    { trigger: "\\\\bra{(.+)}([^ ]+)>", replacement: "\\braket{ [[0]] | $0 ", options: "rmA", description: "Convert bra into braket (alternate)" },
-    { trigger: "outp", replacement: "\\ket{${0:\\psi}} \\bra{${0:\\psi}} $1", options: "mA" },
+    // { trigger: "ot\\mathrm{Im}es", replacement: "\\otimes ", options: "mA" }, // Handle conflict with "im" snippet
+    // { trigger: "bra", replacement: "\\bra{$0} $1", options: "mA" },
+    // { trigger: "ket", replacement: "\\ket{$0} $1", options: "mA" },
+    // { trigger: "brk", replacement: "\\braket{ $0 | $1 } $2", options: "mA" },
+    // { trigger: "\\\\bra{([^|]+)\\|", replacement: "\\braket{ [[0]] | $0 ", options: "rmA", description: "Convert bra into braket" },
+    // { trigger: "\\\\bra{(.+)}([^ ]+)>", replacement: "\\braket{ [[0]] | $0 ", options: "rmA", description: "Convert bra into braket (alternate)" },
+    // { trigger: "outp", replacement: "\\ket{${0:\\psi}} \\bra{${0:\\psi}} $1", options: "mA" },
 
 
 
     // Chemistry
-    { trigger: "pu", replacement: "\\pu{ $0 }", options: "mA" },
-    { trigger: "msun", replacement: "M_{\\odot}", options: "mA" },
-    { trigger: "solm", replacement: "M_{\\odot}", options: "mA" },
-    { trigger: "ce", replacement: "\\ce{ $0 }", options: "mA" },
-    { trigger: "iso", replacement: "{}^{${0:4}}_{${1:2}}${2:He}", options: "mA" },
-    { trigger: "hel4", replacement: "{}^{4}_{2}He ", options: "mA" },
-    { trigger: "hel3", replacement: "{}^{3}_{2}He ", options: "mA" },
+    // { trigger: "pu", replacement: "\\pu{ $0 }", options: "mA" },
+    // { trigger: "msun", replacement: "M_{\\odot}", options: "mA" },
+    // { trigger: "solm", replacement: "M_{\\odot}", options: "mA" },
+    // { trigger: "ce", replacement: "\\ce{ $0 }", options: "mA" },
+    // { trigger: "iso", replacement: "{}^{${0:4}}_{${1:2}}${2:He}", options: "mA" },
+    // { trigger: "hel4", replacement: "{}^{4}_{2}He ", options: "mA" },
+    // { trigger: "hel3", replacement: "{}^{3}_{2}He ", options: "mA" },
 
 
 
@@ -254,28 +280,33 @@
     { trigger: "case", replacement: "\\begin{cases}\n$0\n\\end{cases}", options: "mA" },
     { trigger: "align", replacement: "\\begin{align}\n$0\n\\end{align}", options: "mA" },
     { trigger: "array", replacement: "\\begin{array}\n$0\n\\end{array}", options: "mA" },
-    { trigger: "matrix", replacement: "\\begin{matrix}\n$0\n\\end{matrix}", options: "mA" },
+    // { trigger: "matrix", replacement: "\\begin{matrix}\n$0\n\\end{matrix}", options: "mA" },
 
 
 
     // Brackets
-    { trigger: "avg", replacement: "\\langle $0 \\rangle $1", options: "mA" },
+    // { trigger: "avg", replacement: "\\langle $0 \\rangle $1", options: "mA" },
     { trigger: "norm", replacement: "\\lvert $0 \\rvert $1", options: "mA", priority: 1 },
-    { trigger: "mod", replacement: "|$0|$1", options: "mA" },
+    // { trigger: "mod", replacement: "|$0|$1", options: "mA" },
     { trigger: "(", replacement: "(${VISUAL})", options: "mA" },
     { trigger: "[", replacement: "[${VISUAL}]", options: "mA" },
     { trigger: "{", replacement: "{${VISUAL}}", options: "mA" },
     { trigger: "(", replacement: "($0)$1", options: "mAw" },
     { trigger: "{", replacement: "{$0}$1", options: "mAw" },
     { trigger: "[", replacement: "[$0]$1", options: "mAw" },
+    //! Left Right ( parentheses
     { trigger: "lr(", replacement: "\\left( $0 \\right) $1", options: "mA" },
+    //! Left Right | pipes
     { trigger: "lr|", replacement: "\\left| $0 \\right| $1", options: "mA" },
+    //! Left Right { curly brackets
     { trigger: "lr{", replacement: "\\left\\{ $0 \\right\\} $1", options: "mA" },
+    //! Left Right [ square brackets
     { trigger: "lr[", replacement: "\\left[ $0 \\right] $1", options: "mA" },
+    //! Left Right Angle brackets
     { trigger: "lra", replacement: "\\left< $0 \\right> $1", options: "mA" },
 
 
 
     // Misc
-    { trigger: "tayl", replacement: "${0:f}(${1:x} + ${2:h}) = ${0:f}(${1:x}) + ${0:f}'(${1:x})${2:h} + ${0:f}''(${1:x}) \\frac{${2:h}^{2}}{2!} + \\dots$3", options: "mA" },
+    // { trigger: "tayl", replacement: "${0:f}(${1:x} + ${2:h}) = ${0:f}(${1:x}) + ${0:f}'(${1:x})${2:h} + ${0:f}''(${1:x}) \\frac{${2:h}^{2}}{2!} + \\dots$3", options: "mA" },
 ]
